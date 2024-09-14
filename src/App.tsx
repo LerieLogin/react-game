63b04b362fb00206389e6b7cb777a009f6a98b89
Larry Logan <bubsrocks15@gmail.com>
2024-09-14 10:40:19 -0600

    set up some routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import './App.css'
+import "./App.css";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+import Nav from "./components/Navbar";
+import Home from "./routes/Home";
 
-function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
-export default App
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Nav />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "*", element: <Navigate to="/" /> },
+    ],
+  },
+]);
+
+export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import { NavLink, Outlet } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav>
+        <NavLink to="/">lkirgweniwgrnoi</NavLink>
+      </nav>
+      <Outlet />
+    </>
+  );
+};
+
+export default Navbar;
